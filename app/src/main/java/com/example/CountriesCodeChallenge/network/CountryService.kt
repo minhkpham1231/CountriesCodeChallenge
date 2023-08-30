34de19490773b200ceb656ba8c0428bc2131e626
minhkpham1231 <minhkpham1231@gmail.com>
2023-08-29 17:52:37 -0700

    small warning fix

--- app/src/main/java/com/example/CountriesCodeChallenge/network/CountryService.kt
+++ app/src/main/java/com/example/CountriesCodeChallenge/network/CountryService.kt
@@ -17,7 +17,7 @@ interface CountryService {
         private const val BASE_URL = "https://gist.githubusercontent.com/peymano-wmt/32dcb892b06648910ddd40406e37fdab/raw/db25946fd77c5873b0303b858e861ce724e0dcd0/"
         private const val ENDPOINT = "countries.json"
 
-        val countryService =
+        val countryService: CountryService =
             Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
